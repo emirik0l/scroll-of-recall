918161fb5b69ae205d2acc2a4141381dc1a36c03
Callum Murphy <77687278+emirik0l@users.noreply.github.com>
2024-12-29 13:14:12 +0000

    Implement dynamic models and basic scroll logic

--- src/main/java/net/emirikol/recall/item/RecallScroll.java
+++ src/main/java/net/emirikol/recall/item/RecallScroll.java
@@ -6,7 +6,10 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.world.World;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.CustomModelDataComponent;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -21,30 +24,79 @@ public class RecallScroll extends Item {
 	@Override
 	public ActionResult use(World world, PlayerEntity playerEntity, Hand hand) {
 		ItemStack stack = playerEntity.getStackInHand(hand);
+		
 		switch(this.getStatus(stack)) {
 			case UNBOUND:
-				return this.useUnbound();
+				return this.useUnbound(playerEntity, stack);
 			case RECALL:
-				return this.useRecall();
+				return this.useRecall(playerEntity, stack);
 			case RETURN:
-				return this.useReturn();
+				return this.useReturn(playerEntity, stack);
 		}
 		
 		return ActionResult.SUCCESS;
 	}
 	
-	public ActionResult useUnbound() {
-		System.out.println("Unbound scroll was used!"); //TODO
+	public ActionResult useUnbound(PlayerEntity playerEntity, ItemStack stack) {
+		// TODO - play a sound
+		
+		// Create a scroll of recall.
+		ItemStack newStack = new ItemStack(RecallMod.RECALL_SCROLL, 1);
+		newStack.set(RecallMod.SCROLL_TYPE_COMPONENT, 1);
+		
+		// TODO - store the coordinates of your current location
+		
+		// Give it the correct model.
+		CustomModelDataComponent component = new CustomModelDataComponent(List.of(), List.of(), List.of("recall"), List.of());
+		newStack.set(DataComponentTypes.CUSTOM_MODEL_DATA, component);
+		
+		// Give it to the player.
+		PlayerInventory inventory = playerEntity.getInventory();
+		inventory.offerOrDrop(newStack);
+		inventory.markDirty();
+		
+		// Decrement the old stack.
+		stack.decrement(1);
+		
 		return ActionResult.SUCCESS;
 	}
 	
-	public ActionResult useRecall() {
-		System.out.println("Recall scroll was used!"); //TODO
+	public ActionResult useRecall(PlayerEntity playerEntity, ItemStack stack) {
+		// TODO - play a sound
+		
+		// TODO - make a note of the player's current coordinates ("return coordinates")
+		
+		// TODO - teleport the player to the coordinates stored in the scroll
+		
+		// Create a scroll of return.
+		ItemStack newStack = new ItemStack(RecallMod.RECALL_SCROLL, 1);
+		newStack.set(RecallMod.SCROLL_TYPE_COMPONENT, 2);
+		
+		// TODO - store the return coordinates
+		
+		// Give it the correct model.
+		CustomModelDataComponent component = new CustomModelDataComponent(List.of(), List.of(), List.of("return"), List.of());
+		newStack.set(DataComponentTypes.CUSTOM_MODEL_DATA, component);
+		
+		// Give it to the player.
+		PlayerInventory inventory = playerEntity.getInventory();
+		inventory.offerOrDrop(newStack);
+		inventory.markDirty();
+		
+		// Decrement the old stack.
+		stack.decrement(1);
+		
 		return ActionResult.SUCCESS;
 	}
 	
-	public ActionResult useReturn() {
-		System.out.println("Return scroll was used!"); //TODO
+	public ActionResult useReturn(PlayerEntity playerEntity, ItemStack stack) {
+		// TODO - play a sound
+		
+		// TODO - teleport the player to the coordinates stored in the scroll
+		
+		// Decrement the stack.
+		stack.decrement(1);
+		
 		return ActionResult.SUCCESS;
 	}
 	
